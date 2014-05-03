42d38f3e9cbda07b423bbd4090c16a8ca358fd4f
Ylvakiller <rem.geuze@gmail.com>
2014-05-03 19:28:26 +0200

    First things first, added the console

--- src/runner.java
+++ src/runner.java
@@ -1,8 +1,19 @@
+import javax.swing.JFrame;
+
+import Console.Console;
+
 
 public class runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final Console console = new Console();
+		JFrame jf = new JFrame("Game attempt");
+		jf.setSize(250,351);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.getContentPane().setLayout(null);
+		console.out("Starting this....");
+		
+		jf.setVisible(true);
 
 	}
 
