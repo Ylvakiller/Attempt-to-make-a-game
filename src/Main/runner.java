531d9bdc93cb470088dcda2285d03a641f33c1c7
Ylvakiller <rem.geuze@gmail.com>
2014-05-05 13:39:33 +0200

    Deleted the previous attempt to make a game, will start using LWJGL from now on

--- src/Main/runner.java
+++ src/Main/runner.java
@@ -1,12 +1,8 @@
 package Main;
-import java.awt.EventQueue;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 import Console.Console;
-
-import javax.swing.JButton;
 
 public class runner {
 	static Console console;
@@ -18,31 +14,8 @@ public class runner {
     
     public static void main(String[] a) {
     	
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                
-
-                console = new Console();
-                console.out("testing");
-	                
-                
-                window = new JFrame();
-                window.setSize(840, 560);
-                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                Test test = new Test(window, console);
-                window.getContentPane().add(test);
-                test.setLayout(null);
-                ImageIcon icon = test.createImageIcon("Textures/Button1.png", "just an icon");
-                JButton btnTest = new JButton(icon);
-                
-                btnTest.setBounds(10, 11, 244, 85);
-                test.add(btnTest);
-                window.setVisible(true);
-                
-                
-            }
-        });
+        Console console = new Console();
+        console.out("Lets start somewehre.....");
     }
     
     
